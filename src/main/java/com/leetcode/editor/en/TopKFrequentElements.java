7c6ef961d80346959708c25cf687b4bf68691dd6
tim_man <tim58jsy@outlook.com>
2020-03-23 18:34:43 +0800

    Added java solutions

---
+++ src/main/java/com/leetcode/editor/en/TopKFrequentElements.java
@@ -0,0 +1,74 @@
+//Given a non-empty array of integers, return the k most frequent elements. 
+//
+// Example 1: 
+//
+// 
+//Input: nums = [1,1,1,2,2,3], k = 2
+//Output: [1,2]
+// 
+//
+// 
+// Example 2: 
+//
+// 
+//Input: nums = [1], k = 1
+//Output: [1] 
+// 
+//
+// Note: 
+//
+// 
+// You may assume k is always valid, 1 ≤ k ≤ number of unique elements. 
+// Your algorithm's time complexity must be better than O(n log n), where n is t
+//he array's size. 
+// 
+// Related Topics Hash Table Heap
+
+
+package com.leetcode.editor.en;
+import java.util.*;
+
+public class TopKFrequentElements{
+//    public static void main(String[] args) {
+//       Solution solution = new TopKFrequentElements().new Solution();
+//
+//    }
+  
+    //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+
+    public List<Integer> topKFrequent(int[] nums, int k) {
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+        for(int num: nums){
+            if(map.containsKey(num))
+                map.put(num, map.get(num) + 1);
+            else
+                map.put(num, 1);
+        }
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>(new Comparator<Integer>(){
+            @Override
+            public int compare (Integer o1, Integer o2){
+                return map.get(o1) - map.get(o2);
+            }
+        });
+        for(int key: map.keySet()){
+            if(pq.size() < k)
+                pq.add(key);
+            else if(map.get(key) > map.get(pq.peek())){
+                pq.remove();
+                pq.add(key);
+            }
+        }
+
+        LinkedList<Integer> res = new LinkedList<>();
+        while (!pq.isEmpty())
+            res.add(pq.remove());
+        return res;
+    }
+
+
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
