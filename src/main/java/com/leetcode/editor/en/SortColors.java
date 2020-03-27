7f9987b805fd7fca8df268699378d3659c05e1b3
tim_man <tim58jsy@outlook.com>
2020-03-27 11:09:44 +0800

    Solution

--- src/main/java/com/leetcode/editor/en/SortColors.java
+++ src/main/java/com/leetcode/editor/en/SortColors.java
@@ -33,26 +33,34 @@ public class SortColors{
        Solution solution = new SortColors().new Solution();
        
     }
-  
+
     //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public void sortColors(int[] nums) {
-        int[] count = {0, 0, 0};
-        for(int i = 0; i < nums.length; i++){
-            assert(nums[i] >= 0 && nums[i] <= 2);
-            count[nums[i]] ++;
+        int zero = -1;         //nums[0...zero] == 0
+        int two = nums.length; //nums[two...n-1] == 2
+        for(int i = 0; i < two;){
+            if (nums[i] == 1) {
+                i++;
+            }
+            else if(nums[i] == 0){
+                zero ++;
+                int temp = nums[zero];
+                nums[zero] = nums[i];
+                nums[i] = temp;
+                i++;
+            }
+            else if(nums[i] == 2){
+                two--;
+                int temp = nums[two];
+                nums[two] = nums[i];
+                nums[i] = temp;
+            }
+
         }
-
-        int index = 0;
-        for(int i = 0; i < count[0]; i++)
-            nums[index++] = 0;
-        for(int i = 0; i < count[1]; i++)
-            nums[index++] = 0;
-        for(int i = 0; i < count[2]; i++)
-            nums[index++] = 0;
-
     }
-}
+    }
+
 //leetcode submit region end(Prohibit modification and deletion)
 
 
