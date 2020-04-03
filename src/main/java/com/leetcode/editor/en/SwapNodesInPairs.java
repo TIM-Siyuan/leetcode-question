26af9e7fcd87f25ee821a9f8c409fa8d7ee0e30f
tim_man <tim58jsy@outlook.com>
2020-04-03 11:17:36 +0800

    Solution

---
+++ src/main/java/com/leetcode/editor/en/SwapNodesInPairs.java
@@ -0,0 +1,55 @@
+//Given a linked list, swap every two adjacent nodes and return its head. 
+//
+// You may not modify the values in the list's nodes, only nodes itself may be c
+//hanged. 
+//
+// 
+//
+// Example: 
+//
+// 
+//Given 1->2->3->4, you should return the list as 2->1->4->3.
+// 
+// Related Topics Linked List
+
+
+package com.leetcode.editor.en;
+
+public class SwapNodesInPairs{
+    public static void main(String[] args) {
+       Solution solution = new SwapNodesInPairs().new Solution();
+       
+    }
+  
+    //leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+        ListNode dummyhead = new ListNode(0);
+        dummyhead.next = head;
+        ListNode p = dummyhead;
+        while(p.next != null && p.next.next != null){
+            ListNode node1 = p.next;
+            ListNode node2 = node1.next;
+            ListNode next = node2.next;
+
+            node2.next = node1;
+            node1.next = next;
+            p.next = node2;
+
+            p = node1;
+        }
+        return dummyhead.next;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+
+}
