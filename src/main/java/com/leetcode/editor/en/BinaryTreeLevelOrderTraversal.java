468513db12f02df6b4b5abcc80362d459d56add2
tim_man <tim58jsy@outlook.com>
2020-04-08 01:12:49 +0800

    Solution

---
+++ src/main/java/com/leetcode/editor/en/BinaryTreeLevelOrderTraversal.java
@@ -0,0 +1,86 @@
+//Given a binary tree, return the level order traversal of its nodes' values. (i
+//e, from left to right, level by level). 
+//
+// 
+//For example: 
+//Given binary tree [3,9,20,null,null,15,7], 
+// 
+//    3
+//   / \
+//  9  20
+//    /  \
+//   15   7
+// 
+// 
+// 
+//return its level order traversal as: 
+// 
+//[
+//  [3],
+//  [9,20],
+//  [15,7]
+//]
+// 
+// Related Topics Tree Breadth-first Search
+
+
+package com.leetcode.editor.en;
+
+import javafx.util.Pair;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class BinaryTreeLevelOrderTraversal{
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+    public static void main(String[] args) {
+       Solution solution = new BinaryTreeLevelOrderTraversal().new Solution();
+       
+    }
+  
+    //leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        ArrayList<List<Integer>> res = new ArrayList<List<Integer>>();
+
+        if(root == null)
+            return res;
+        LinkedList<Pair<TreeNode, Integer>> queue = new LinkedList<Pair<TreeNode, Integer>>();
+        queue.addLast(new Pair<TreeNode, Integer>(root, 0));
+
+        while(!queue.isEmpty()){
+            Pair<TreeNode, Integer> front = queue.removeFirst();
+            TreeNode node = front.getKey();
+            int level = front.getValue();
+
+            if(level == res.size())
+                res.add(new ArrayList<Integer>());
+            assert level < res.size();
+
+            res.get(level).add(node.val);
+            if(node.left != null)
+                queue.addLast(new Pair<TreeNode, Integer>(node.left, level + 1));
+            if(node.right != null)
+                queue.addLast(new Pair<TreeNode, Integer>(node.right, level + 1));
+        }
+        return res;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+
+}
