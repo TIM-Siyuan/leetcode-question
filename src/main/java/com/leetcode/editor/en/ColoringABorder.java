9aaec76b5632f0e5b048a2cc5e591a63bbb15841
tim_man <tim58jsy@outlook.com>
2020-04-18 00:38:20 +0800

    Solution

---
+++ src/main/java/com/leetcode/editor/en/ColoringABorder.java
@@ -0,0 +1,122 @@
+//Given a 2-dimensional grid of integers, each value in the grid represents the 
+//color of the grid square at that location. 
+//
+// Two squares belong to the same connected component if and only if they have t
+//he same color and are next to each other in any of the 4 directions. 
+//
+// The border of a connected component is all the squares in the connected compo
+//nent that are either 4-directionally adjacent to a square not in the component, 
+//or on the boundary of the grid (the first or last row or column). 
+//
+// Given a square at location (r0, c0) in the grid and a color, color the border
+// of the connected component of that square with the given color, and return the 
+//final grid. 
+//
+// 
+//
+// Example 1: 
+//
+// 
+//Input: grid = [[1,1],[1,2]], r0 = 0, c0 = 0, color = 3
+//Output: [[3, 3], [3, 2]]
+// 
+//
+// 
+// Example 2: 
+//
+// 
+//Input: grid = [[1,2,2],[2,3,2]], r0 = 0, c0 = 1, color = 3
+//Output: [[1, 3, 3], [2, 3, 3]]
+// 
+//
+// 
+// Example 3: 
+//
+// 
+//Input: grid = [[1,1,1],[1,1,1],[1,1,1]], r0 = 1, c0 = 1, color = 2
+//Output: [[2, 2, 2], [2, 1, 2], [2, 2, 2]] 
+// 
+// 
+//
+// 
+//
+// Note: 
+//
+// 
+// 1 <= grid.length <= 50 
+// 1 <= grid[0].length <= 50 
+// 1 <= grid[i][j] <= 1000 
+// 0 <= r0 < grid.length 
+// 0 <= c0 < grid[0].length 
+// 1 <= color <= 1000 
+// Related Topics Depth-first Search
+
+
+package com.leetcode.editor.en;
+
+import java.net.HttpURLConnection;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class ColoringABorder{
+    public static void main(String[] args) {
+       Solution solution = new ColoringABorder().new Solution();
+       
+    }
+//  tips: 使用队列, while只压入四向联通的点, 被孤立断开的点不会继续遍历;
+//  使用两个for循环则会遍历所有的点, floodfill即使断开了也会遍历; 因为if不能屏蔽断开的
+//  此题while, 需要设立isBorder标识, 标记边界的点，所以即使孤立了最后遍历时也会被改; 最后使用for循环再遍历所有的点, 效率低;
+//  用两个for循环，也需要标记;
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    private int[][] dirs = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+    private boolean[][] visited;
+    private int[][] grid;
+    private boolean[][] border;
+    public int[][] colorBorder(int[][] grid, int r0, int c0, int color) {
+        int oldColor = grid[r0][c0];
+        if(oldColor == color) return grid;
+        this.grid = grid;
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[] {r0, c0});
+        visited = new boolean[grid.length][grid[0].length];
+        border = new boolean[grid.length][grid[0].length];
+        visited[r0][c0] = true;
+
+        while (!queue.isEmpty()){
+            int[] temp = queue.remove();
+            int x = temp[0];
+            int y = temp[1];
+            boolean isBorder = false;
+            for(int d = 0; d < dirs.length; d++){
+                int nextx = x + dirs[d][0]; int nexty = y + dirs[d][1];
+                if(inArea(nextx, nexty) && grid[nextx][nexty] == oldColor && !visited[nextx][nexty]){
+                    queue.add(new int[] {nextx, nexty});
+                    visited[nextx][nexty] = true;
+                }
+                if(checkBorder(nextx, nexty) || grid[nextx][nexty] != oldColor) isBorder = true;
+            }
+
+            if(isBorder) border[x][y] = isBorder;
+        }
+
+        for(int i = 0; i < grid.length; i++){
+            for(int j = 0; j < grid[0].length; j++){
+                if(border[i][j]) grid[i][j] = color;
+            }
+        }
+        return grid;
+    }
+
+    private boolean inArea(int x, int y){
+        return x >= 0 && x < grid.length && y >= 0 && y < grid[0].length;
+    }
+    private boolean checkBorder(int x, int y){
+        return x < 0 || y < 0 || x >= grid.length || y >= grid[0].length;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+
+}
--- src/main/java/com/leetcode/editor/en/FloodFill.java
+++ src/main/java/com/leetcode/editor/en/FloodFill.java
@@ -52,8 +52,9 @@ public class FloodFill{
   
     //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
-//  法一：没法加visited，效率不高
-    /*private int[][] dirs = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+//  法一: 使用queue,所以不是四联通的不会入队；使用双重for循环会遍历所有，将不是四联通的也改了
+    private int[][] dirs = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+    private boolean[][] visited;
     private int[][] image;
     public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
         int oldColor = image[sr][sc];
@@ -61,16 +62,20 @@ class Solution {
         if(oldColor == newColor) return image;
         Queue<int[]> queue = new LinkedList<>();
         queue.add(new int[] {sr, sc});
+        visited = new boolean[image.length][image[0].length];
         while(!queue.isEmpty()){
             int[] temp = queue.poll();
             int x = temp[0]; int y = temp[1];
             image[x][y] = newColor;
             for(int d = 0; d < dirs.length; d++){
                 int nextx = x + dirs[d][0], nexty = y + dirs[d][1];
-                if(!inArea(nextx, nexty) || image[nextx][nexty] != oldColor) {
+                /*if(!inArea(nextx, nexty) || image[nextx][nexty] != oldColor) {
                     continue;
+                }*/
+                if(inArea(nextx, nexty) && image[nextx][nexty] == oldColor && !visited[nextx][nexty]){
+                    visited[nextx][nexty] = true;
+                    queue.add(new int[]{nextx, nexty});
                 }
-                queue.add(new int[]{nextx, nexty});
             }
         }
         return image;
@@ -78,8 +83,8 @@ class Solution {
 
     private boolean inArea(int x, int y){
         return x >= 0 && x < image.length && y >= 0 && y < image[0].length;
-    }*/
-
+    }
+/*
 //  法二：递归，用for循环效率比四个if低
 //    private int[][] dirs = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
 //    private int[][] image;
@@ -92,25 +97,25 @@ class Solution {
         return image;
     }
 
-  /*  private boolean inArea(int x, int y){
+  *//*  private boolean inArea(int x, int y){
         return x >= 0 && x < image.length && y >= 0 && y < image[0].length;
-    }*/
+    }*//*
 
     private void helper(int[][] image, int sr, int sc, int newColor, int oldColor){
         if(image[sr][sc] == oldColor){
             image[sr][sc] = newColor;
-            /*for(int d = 0; d < dirs.length; d++){
+            *//*for(int d = 0; d < dirs.length; d++){
                 int x = sr + dirs[d][0]; int y = sc + dirs[d][1];
                 if(inArea(x, y)){
                     helper(image, x, y, newColor, oldColor);
                 }
-            }*/
+            }*//*
             if(sr-1 >= 0) helper(image, sr-1, sc, newColor, oldColor);
             if(sc-1 >= 0) helper(image, sr, sc-1, newColor, oldColor);
             if(sr+1 < image.length) helper(image, sr+1, sc, newColor, oldColor); //此处要用小于号，因为从0计数，sr+1 < length
             if(sc+1 < image[0].length) helper(image, sr, sc+1, newColor, oldColor);
         }
-    }
+    }*/
 }
 //leetcode submit region end(Prohibit modification and deletion)
 
