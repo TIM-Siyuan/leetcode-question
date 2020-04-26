3fed17c7d0b74b2a027e86d3795135be9ec06909
tim_man <tim58jsy@outlook.com>
2020-04-26 22:02:44 +0800

    Solution

---
+++ src/main/java/com/leetcode/editor/en/HouseRobberIii.java
@@ -0,0 +1,107 @@
+//The thief has found himself a new place for his thievery again. There is only 
+//one entrance to this area, called the "root." Besides the root, each house has o
+//ne and only one parent house. After a tour, the smart thief realized that "all h
+//ouses in this place forms a binary tree". It will automatically contact the poli
+//ce if two directly-linked houses were broken into on the same night. 
+//
+// Determine the maximum amount of money the thief can rob tonight without alert
+//ing the police. 
+//
+// Example 1: 
+//
+// 
+//Input: [3,2,3,null,3,null,1]
+//
+//     3
+//    / \
+//   2   3
+//    \   \ 
+//     3   1
+//
+//Output: 7 
+//Explanation: Maximum amount of money the thief can rob = 3 + 3 + 1 = 7. 
+//
+// Example 2: 
+//
+// 
+//Input: [3,4,5,1,3,null,1]
+//
+//     3
+//    / \
+//   4   5
+//  / \   \ 
+// 1   3   1
+//
+//Output: 9
+//Explanation: Maximum amount of money the thief can rob = 4 + 5 = 9.
+// Related Topics Tree Depth-first Search
+
+
+package com.leetcode.editor.en;
+
+import java.util.HashMap;
+
+public class HouseRobberIii{
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+    public static void main(String[] args) {
+       Solution solution = new HouseRobberIii().new Solution();
+       
+    }
+  
+    //leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    HashMap<TreeNode, Integer> memo = new HashMap<TreeNode, Integer>();
+    public int rob(TreeNode root) {
+        if(root == null) return 0;
+        if(memo.containsKey(root)) return memo.get(root);
+
+        int do_it = root.val
+                + (root.left == null ? 0 : rob(root.left.left) + rob(root.left.right))
+                + (root.right == null ? 0: rob(root.right.left) + rob(root.right.right));
+        int not_do = rob(root.left) + rob(root.right);
+
+        int res = Math.max(do_it, not_do);
+        memo.put(root, res);
+
+        return memo.get(root);
+    }
+
+ /*   //不需要备忘录
+    public int rob(TreeNode root){
+        int[] res = dp(root);
+        return Math.max(res[0], res[1]);
+    }
+
+     返回一个大小为 2 的数组 arr
+    arr[0] 表示不抢 root 的话，得到的最大钱数
+    arr[1] 表示抢 root 的话，得到的最大钱数
+    private int[] dp(TreeNode root){
+        if(root == null) return new int[]{0, 0};
+        int[] left = dp(root.left);
+        int[] right = dp(root.right);
+
+        int rob = root.val + left[0] + right[0];
+        int not_rob = Math.max(left[0], left[1]) + Math.max(right[0], right[1]);
+
+        return new int[]{not_rob, rob};
+    }*/
+
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+
+}
