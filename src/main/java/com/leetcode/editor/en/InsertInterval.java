c99f58b69bf7bae7d1f95ee65fa21fea956a0917
tim_man <tim58jsy@outlook.com>
2020-05-06 19:45:21 +0800

    Solution

---
+++ src/main/java/com/leetcode/editor/en/InsertInterval.java
@@ -0,0 +1,69 @@
+//Given a set of non-overlapping intervals, insert a new interval into the inter
+//vals (merge if necessary). 
+//
+// You may assume that the intervals were initially sorted according to their st
+//art times. 
+//
+// Example 1: 
+//
+// 
+//Input: intervals = [[1,3],[6,9]], newInterval = [2,5]
+//Output: [[1,5],[6,9]]
+// 
+//
+// Example 2: 
+//
+// 
+//Input: intervals = [[1,2],[3,5],[6,7],[8,10],[12,16]], newInterval = [4,8]
+//Output: [[1,2],[3,10],[12,16]]
+//Explanation: Because the new interval [4,8] overlaps with [3,5],[6,7],[8,10]. 
+//
+//
+// NOTE: input types have been changed on April 15, 2019. Please reset to defaul
+//t code definition to get new method signature. 
+// Related Topics Array Sort
+
+
+package com.leetcode.editor.en;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class InsertInterval{
+    public static void main(String[] args) {
+       Solution solution = new InsertInterval().new Solution();
+       int[][] nums = {{1, 2}, {3, 5}, {6, 7}, {8, 10}, {12, 16}};
+       int[] num = {4, 8};
+       solution.insert(nums, num);
+    }
+  
+    //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int[][] insert(int[][] intervals, int[] newInterval) {
+        int start = newInterval[0], end = newInterval[1];
+        List<int[]> res = new ArrayList<int[]>();
+        int index = 0;
+        //left
+        while(index < intervals.length && intervals[index][1] < start){
+            res.add(intervals[index++]);
+        }
+        //Merger overlapping
+        while(index < intervals.length && intervals[index][0] <= end){
+            int[] curr = intervals[index++];
+            start = Math.min(curr[0], start);
+            end = Math.max(curr[1], end);
+        }
+        res.add(new int[]{start, end});
+
+        //right
+        while(index < intervals.length){
+            res.add(intervals[index++]);
+        }
+
+        return res.toArray(new int[res.size()][]);
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+
+}
