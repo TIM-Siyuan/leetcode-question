1d73669596b6590184d98c223d724c3118eebdfa
tim_man <tim58jsy@outlook.com>
2020-05-07 15:28:59 +0800

    Solution

---
+++ src/main/java/com/leetcode/editor/en/MergeTwoSortedLists.java
@@ -0,0 +1,69 @@
+//Merge two sorted linked lists and return it as a new list. The new list should
+// be made by splicing together the nodes of the first two lists. 
+//
+// Example:
+// 
+//Input: 1->2->4, 1->3->4
+//Output: 1->1->2->3->4->4
+// 
+// Related Topics Linked List
+
+
+package com.leetcode.editor.en;
+
+public class MergeTwoSortedLists{
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) { val = x; }
+    }
+    public static void main(String[] args) {
+       Solution solution = new MergeTwoSortedLists().new Solution();
+       
+    }
+  
+    //leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+         /*ListNode dummy = new ListNode(0);
+        ListNode cur = dummy;
+        while (l1 != null && l2 != null){
+            if(l1.val < l2.val){
+                cur.next = l1;
+                l1 = l1.next;
+            }
+            else{
+                cur.next = l2;
+                l2 = l2.next;
+            }
+            cur = cur.next;
+        }
+        if(l1 != null) cur.next = l1;
+        if(l2 != null) cur.next = l2;
+        return dummy.next;*/
+
+        if(l1 == null) return l2;
+        if(l2 == null) return l1;
+        if(l1.val < l2.val){
+            l1.next = mergeTwoLists(l1.next, l2);
+            return l1;
+        }
+        else {
+            l2.next = mergeTwoLists(l1, l2.next);
+            return l2;
+        }
+        }
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+
+}
