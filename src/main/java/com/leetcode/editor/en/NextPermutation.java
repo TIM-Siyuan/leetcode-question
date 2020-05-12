f1ad114916ad42e9e2dc66e526406ab6082a30c0
tim_man <tim58jsy@outlook.com>
2020-05-12 22:50:17 +0800

    Solution

--- src/main/java/com/leetcode/editor/en/NextPermutation.java
+++ src/main/java/com/leetcode/editor/en/NextPermutation.java
@@ -34,9 +34,11 @@ class Solution {
         while (i >= 0 && nums[i] >= nums[i + 1]) i--;
         if(i >= 0){
             int j = nums.length - 1;
-            int l = i + 1, r = nums.length;
-            while (nums[j] <= nums[i]) j--;
-            swap(nums, i, j);
+            int l = i + 1, r = nums.length - 1;
+            //可以使用二分寻找大于nums[i]的第一个数; 但是此处二分写法有问题, bug还没解决
+            int next = binary(nums, i + 1, r, nums[i]);
+//            while (nums[j] <= nums[i]) j--;
+            swap(nums, i, next);
         }
         reverse(nums, i + 1, nums.length - 1);
     }
@@ -49,6 +51,30 @@ class Solution {
 
     private void reverse(int[] nums, int i, int j){
         while (i < j) swap(nums, i++, j--);
+    }
+
+    private int binary(int[] num, int start, int end, int k){
+        if(end - start + 1 == 1) return start;
+        int l = start, r = end;
+        while (l < r) {
+            int mid = (l + r) / 2;
+            if(num[mid] == k) {
+                while (num[mid] == k) mid--;
+                return mid;
+            }
+            else if(num[mid] > k){
+                if(num[mid + 1] <= k){
+                    return mid;
+                }
+                else {
+                    l = mid + 1;
+                }
+            }
+            else {
+                r = mid + 1;
+            }
+        }
+        return l;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
