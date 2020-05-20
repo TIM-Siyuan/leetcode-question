5eaaeaa6792dfe0bf9459dec53ab7b49c3e16ec4
tim_man <tim58jsy@outlook.com>
2020-05-20 19:53:29 +0800

    Solution

--- src/main/java/com/leetcode/editor/en/MinimumDepthOfBinaryTree.java
+++ src/main/java/com/leetcode/editor/en/MinimumDepthOfBinaryTree.java
@@ -22,7 +22,17 @@
 
 package com.leetcode.editor.en;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class MinimumDepthOfBinaryTree{
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+
     public static void main(String[] args) {
        Solution solution = new MinimumDepthOfBinaryTree().new Solution();
        
@@ -40,14 +50,8 @@ public class MinimumDepthOfBinaryTree{
  */
 
 class Solution {
-    public class TreeNode {
-        int val;
-        TreeNode left;
-        TreeNode right;
-        TreeNode(int x) { val = x; }
-    }
-
-    public int minDepth(TreeNode root) {
+    //递归DFS
+    /*public int minDepth(TreeNode root) {
         if(root == null) return 0;
         if(root.left == null && root.right == null) return 1;
 
@@ -55,6 +59,32 @@ class Solution {
         if(root.right == null) return minDepth(root.left) + 1;
 
         return Math.min(minDepth(root.left), minDepth(root.right)) + 1;
+    }*/
+
+    //BFS
+    public int minDepth(TreeNode root){
+        if(root == null) return 0;
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        // root 本身就是一层，depth 初始化为 1
+        int depth = 1;
+        while (!q.isEmpty()){
+            int sz = q.size();
+            for(int i = 0; i < sz; i++){
+                TreeNode cur = q.poll();
+                /* 判断是否到达终点 */
+                if(cur.left == null && cur.right == null)
+                    return depth;
+                /* 将 cur 的相邻节点加入队列 */
+                if(cur.left != null)
+                    q.offer(cur.left);
+                if(cur.right != null)
+                    q.offer(cur.right);
+            }
+            /* 这里增加步数 */
+            depth++;
+        }
+        return depth;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
