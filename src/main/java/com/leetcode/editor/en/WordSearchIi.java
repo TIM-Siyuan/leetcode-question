8f8311bd4c0ab5bb24409544f0efd17b7dee8570
tim_man <tim58jsy@outlook.com>
2020-06-25 10:29:33 +0800

    Solution

--- src/main/java/com/leetcode/editor/en/WordSearchIi.java
+++ src/main/java/com/leetcode/editor/en/WordSearchIi.java
@@ -72,7 +72,9 @@ class Solution {
 
     public List<String> findWords(char[][] board, String[] words) {
         List<String> res = new ArrayList<>();
-        if(board == null || board.length == 0) return res;
+        if(board == null || board.length == 0){
+            return res;
+        }
         //char字符'\0'表示为空
         TrieNode root = new TrieNode('\0');
         buildTrie(root, words);
@@ -88,11 +90,18 @@ class Solution {
     }
 
     private void dfs(char[][] board, int i, int j, TrieNode cur, List<String> res){
-        if(i < 0 || i >= board.length || j < 0 || j >= board[0].length) return;
+        if(i < 0 || i >= board.length || j < 0 || j >= board[0].length) {
+            return;
+        }
 
         char c = board[i][j];
-        if(c == '*') return;
-        if(cur.children[c - 'a'] == null) return;
+        if(c == '*') {
+            return;
+        }
+
+        if(cur.children[c - 'a'] == null) {
+            return;
+        }
 
         cur = cur.children[c - 'a'];
         if(cur.word != null){
@@ -108,6 +117,54 @@ class Solution {
         dfs(board, i, j - 1, cur, res);
         board[i][j] = c;
     }
+
+
+    /*public List<String> findWords(char[][] board, String[] words) {
+        List<String> res = new ArrayList<>();
+        TrieNode root = buildTrie(words);
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                dfs (board, i, j, root, res);
+            }
+        }
+        return res;
+    }
+
+    public void dfs(char[][] board, int i, int j, TrieNode p, List<String> res) {
+        char c = board[i][j];
+        if (c == '#' || p.next[c - 'a'] == null) return;
+        p = p.next[c - 'a'];
+        if (p.word != null) {   // found one
+            res.add(p.word);
+            p.word = null;     // de-duplicate
+        }
+
+        board[i][j] = '#';
+        if (i > 0) dfs(board, i - 1, j ,p, res);
+        if (j > 0) dfs(board, i, j - 1, p, res);
+        if (i < board.length - 1) dfs(board, i + 1, j, p, res);
+        if (j < board[0].length - 1) dfs(board, i, j + 1, p, res);
+        board[i][j] = c;
+    }
+
+    public TrieNode buildTrie(String[] words) {
+        TrieNode root = new TrieNode();
+        for (String w : words) {
+            TrieNode p = root;
+            for (char c : w.toCharArray()) {
+                int i = c - 'a';
+                if (p.next[i] == null) p.next[i] = new TrieNode();
+                p = p.next[i];
+            }
+            p.word = w;
+        }
+        return root;
+    }
+
+    class TrieNode {
+        TrieNode[] next = new TrieNode[26];
+        String word;
+    }*/
 }
 //leetcode submit region end(Prohibit modification and deletion)
 
