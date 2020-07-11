4c2f71a255a41e49f31b8f01dc44e97c68029272
tim_man <tim58jsy@outlook.com>
2020-07-12 00:19:55 +0800

    Solution

--- src/main/java/com/leetcode/editor/en/SlidingPuzzle.java
+++ src/main/java/com/leetcode/editor/en/SlidingPuzzle.java
@@ -54,10 +54,7 @@
 
 package com.leetcode.editor.en;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.*;
 
 public class SlidingPuzzle{
     public static void main(String[] args) {
@@ -67,7 +64,7 @@ public class SlidingPuzzle{
   
     //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
-    private int[][] dirs = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+    /*private int[][] dirs = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
     public int slidingPuzzle(int[][] board) {
         Queue<String> queue = new LinkedList<>();
         HashMap<String, Integer> visited = new HashMap<>();
@@ -140,6 +137,54 @@ class Solution {
             board[i / 3][i % 3] = s.charAt(i) - '0';
         }
         return board;
+    }*/
+
+    public int slidingPuzzle(int[][] board){
+        String target = "123450";
+        String start = "";
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                start += board[i][j];
+            }
+        }
+        HashSet<String> visited = new HashSet<>();
+        // all the positions 0 can be swapped to
+        int[][] dirs = new int[][] { { 1, 3 }, { 0, 2, 4 },
+                { 1, 5 }, { 0, 4 }, { 1, 3, 5 }, { 2, 4 } };
+        Queue<String> queue = new LinkedList<>();
+        queue.offer(start);
+        visited.add(start);
+        int res = 0;
+        while (!queue.isEmpty()) {
+            // level count, has to use size control here, otherwise not needed
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                String cur = queue.poll();
+                if (cur.equals(target)) {
+                    return res;
+                }
+                int zero = cur.indexOf('0');
+                // swap if possible
+                for (int dir : dirs[zero]) {
+                    String next = swap(cur, zero, dir);
+                    if (visited.contains(next)) {
+                        continue;
+                    }
+                    visited.add(next);
+                    queue.offer(next);
+
+                }
+            }
+            res++;
+        }
+        return -1;
+    }
+
+    private String swap(String str, int i, int j) {
+        StringBuilder sb = new StringBuilder(str);
+        sb.setCharAt(i, str.charAt(j));
+        sb.setCharAt(j, str.charAt(i));
+        return sb.toString();
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
