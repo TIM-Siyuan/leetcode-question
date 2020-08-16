748043a16c3bbceaf957409b17d55921637cc243
tim_man <tim58jsy@outlook.com>
2020-08-16 10:38:25 +0800

    Solution

--- src/main/java/com/leetcode/editor/en/JumpGameIi.java
+++ src/main/java/com/leetcode/editor/en/JumpGameIi.java
@@ -30,14 +30,18 @@ public class JumpGameIi{
   
     //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
+    // Greedy ≈ implicit BFS, 每一层找到能跳的最远的点
     public int jump(int[] nums) {
         int n = nums.length;
-        int end = 0, farthest = 0;
-        int jumps = 0;
+        int jumps = 0, end = 0, farthest = 0;
+        // 最后一个index不用计算所以n-1
         for(int i = 0; i < n - 1; i++){
             farthest = Math.max(nums[i] + i, farthest);
+            // 每一层找到能跳的最远的点
+            // i == curEnd means you visited all the items on the current level
             if (end == i) {
                 jumps++;
+                // curEnd = curFarthest is like getting the queue size (level size) for the next level you are traversing.
                 end = farthest;
             }
         }
