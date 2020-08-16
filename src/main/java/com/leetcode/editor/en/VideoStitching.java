6171d9b1e6f3bacd25eb56f7003e7b966e6e002b
tim_man <tim58jsy@outlook.com>
2020-08-16 11:59:02 +0800

    Solution

--- src/main/java/com/leetcode/editor/en/VideoStitching.java
+++ src/main/java/com/leetcode/editor/en/VideoStitching.java
@@ -93,14 +93,16 @@ class Solution {
         return res;
     }
 
-    // DP
+    // DP dp[i] is the min number of clips to cover video
     /*public int videoStitching(int[][] clips, int T){
         int[] dp = new int[T + 1];
         Arrays.fill(dp, T + 1);
         dp[0] = 0;
         for(int i = 1; i <= T && dp[i - 1] < T; ++i){
             for(int[] c : clips){
+                // i in [c[0], c[1]]
                 if(c[0] <= i && i <= c[1]){
+                    // check if this video can be covered using less clips
                     dp[i] = Math.min(dp[i], dp[c[0]] + 1);
                 }
             }
